576e7fbdc142b8964dff43e15b4376fb63a50bec
Daria Zherebtsova <dd_mail@inbox.ru>
2020-06-28 17:23:57 +0300

    module-3 task-1 and task-2

--- 3-module/2-task/components/DropdownButton.vue
+++ 3-module/2-task/components/DropdownButton.vue
@@ -1,18 +1,26 @@
 <template>
   <div class="dropdown show">
-    <button type="button" class="button dropdown__toggle dropdown__toggle_icon">
-      <app-icon icon="coffee" />
-      Заголовок - два
+    <button
+      type="button"
+      class="button dropdown__toggle"
+      :class="{ dropdown__toggle_icon: haveIcon }"
+      @click="fClose = !fClose"
+    >
+      <app-icon v-if="haveIcon" :icon="currentIcon" />
+      {{ currentTitle }}
     </button>
 
-    <div class="dropdown__menu show">
-      <button class="dropdown__item dropdown__item_icon" type="button">
-        <app-icon icon="coffee" />
-        раз
-      </button>
-      <button class="dropdown__item dropdown__item_icon" type="button">
-        <app-icon icon="coffee" />
-        два
+    <div class="dropdown__menu" :class="{ show: !fClose }">
+      <button
+        v-for="item in options"
+        :key="item.value"
+        type="button"
+        class="dropdown__item"
+        :class="{ dropdown__item_icon: item.icon }"
+        @click="onButtonClick(item)"
+      >
+        <app-icon v-if="item.icon" :icon="item.icon" />
+        {{ item.text }}
       </button>
     </div>
   </div>
@@ -25,7 +33,53 @@ export default {
   name: 'DropdownButton',
 
   components: { AppIcon },
+
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    options: {
+      type: Array,
+      required: true,
+    },
+    value: {
+      type: String,
+    },
+  },
+
+  model: {
+    prop: 'value',
+    event: 'change',
+  },
+
+  data: function () {
+    return {
+      fClose: true,
+      currentIcon: 'key',
+      currentValueText: '',
+    };
+  },
+
+  computed: {
+    haveIcon() {
+      return this.options.some((item) => item.icon);
+    },
+
+    currentTitle() {
+      return this.value ? `${this.title} - ${this.value}` : this.title;
+    },
+  },
+
+  methods: {
+    onButtonClick(item) {
+      this.$emit('change', item.value);
+      this.fClose = true;
+      this.currentIcon = item.icon;
+    },
+  },
 };
 </script>
 
-<style scoped></style>
+<style src="../../../public/assets/styles/_button.css" scoped></style>
+<style src="../../../public/assets/styles/_dropdown.css" scoped></style>
